79fafcb723d1297f2cd5a3eb5f9cef7fc235a449
ShivamlNikam <shivam.nikam@somaiya.edu>
2022-11-28 01:24:16 +0530

    final

--- backend/routes/post.js
+++
@@ -1,8 +0,0 @@
-// we will make apis in this, use functions that have been made in 'controllers' folder
-const express = require("express");
-const { createPost } = require("../controllers/post");
-const router = express.Router();
-
-router.route("/posts/upload").post(createPost);
-
-module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,52 @@
-const app = require('./app');
-const { connectDatabase } = require("./config/database");
+const bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+const posts = require('./routes/postRoute');
+const users = require('./routes/userRoute');
+const dbURI = process.env.REACT_APP_DB_URI || require('./secrets').dbURI;
 
-connectDatabase();
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () => {
-    console.log(`The server is running on port ${process.env.PORT}`);
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  return next();
+});
+
+mongoose
+  .connect(
+    dbURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('Failed to connect to MongoDB', err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'));
+app.use('/posts', posts);
+app.use('/users', users);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
